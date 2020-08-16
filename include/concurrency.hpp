e3aebe537576039d55ab498586ee12378d3e28cf
dan <danartenie158@gmail.com>
2020-08-16 14:53:19 +0000

    changed folder structure

---
+++ include/concurrency.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "async.hpp"
+#include "thread.hpp"
+#include "execution_context.hpp"
---
+++ include/execution_context.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "static_context.hpp"
+#include "dynamic_context.hpp"
